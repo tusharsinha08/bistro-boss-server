fa6009bbdc21040a7185620fd1cdbb6be4bb9a74
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2025-08-09 08:34:40 +0600

    payment apis added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
+
+
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
-require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -32,6 +35,7 @@ async function run() {
         const menuCollection = database.collection('menu')
         const reviewCollection = database.collection('reviews')
         const cartCollection = database.collection('carts')
+        const paymentCollection = database.collection('payments')
 
 
         // Connect the client to the server	(optional starting in v4.7)
@@ -197,6 +201,35 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query)
             res.send(result)
+        })
+
+        // payment system-------------
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body
+            const amount = parseInt(price * 100)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body
+            const paymentResult = await paymentCollection.insertOne(payment)
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            }
+
+            const deleteResult = await cartCollection.deleteMany(query)
+
+            res.send({ paymentResult, deleteResult })
         })
 
         // Send a ping to confirm a successful connection
